306bfe76ff4ce0e49a88fee0581f2a0961a85bed
webr1 <mkubaev24@gmail.com>
2025-11-27 13:39:39 +0500

    da

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,9 +1,11 @@
 from pydantic_settings import BaseSettings
+import os
+from dotenv import load_dotenv
 
 class Settings(BaseSettings):
     app_name: str = "TRIKCHI_UZ"
-    debug: bool = True
-    database_url: str = "sqlite:///./trikchi_data.db"
+    debug: bool
+    database_url: str 
     cors_origins: list = [
         # Порты для фронтенда
         ]
@@ -12,15 +14,16 @@ class Settings(BaseSettings):
     image_dir: str = "backend/app/static/image"
 
     # Для хеширование пароли
-    SECRET_KEY: str = 'ibdwqyabcqywcwuibiWSBIasgddYEYEBWOFJBVEQO7BIPSbcybbyby3ub2ib2ibfedbcs7cb'
+    SECRET_KEY: str 
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
     REFRESH_TOKEN_EXPIRE_DAYS: int = 30 
 
     #Для хронения секретных ключев
-    GOOGLE_CLIENT_ID: str = "534325035703-t431d22s1mdrl596u76uikbd9mov36gk.apps.googleusercontent.com"
-    GOOGLE_CLIENT_SECRET_KEY: str = "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"
-    GOOGLE_REDIRECT_URI: str = "http://127.0.0.1:8000/auth/google/callback/"
+    GOOGLE_CLIENT_ID: str 
+    GOOGLE_CLIENT_SECRET_KEY: str 
+    GOOGLE_REDIRECT_URI: str 
+
 
 
 
